4817b95580e7ac51156e905a5776c39236aacd34
王永峰 <wangyongfeng03@meituan.com>
2019-11-05 20:04:42 +0800

    增加loading

---
+++ src/component/Loading/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.less';
+
+export default function Loading() {
+  return (
+    <svg width="36" height="36" viewBox="0 0 50 50" className="a-loading-android">
+      <circle cx="25" cy="25" r="20" fill="none" stroke="currentColor" stroke-width="5"></circle>
+    </svg>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import List from '@/component/List';
+import Loading from '@/component/Loading';
 import './index.less';
 
 export default class index extends Component {
   render() {
     return (
       <div className="page-home">
+        <Loading></Loading>
         <List itemCount={100} itemSize={200} header="haha" headerSize={30}>
           {index => <div>{index}</div>}
         </List>
